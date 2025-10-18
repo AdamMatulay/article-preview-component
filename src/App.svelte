5217e345a979a9a92034f993d8ec6f4a49fc1948
AdamMatulay <adammatulay@proton.me>
2025-10-18 23:31:44 +0200

    HTML finished + css base

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,51 @@
+<script>
+    
+</script>
 
 <main>
-  
+  <article class="article-preview-card">
+    
+    <div class="article-image">
+        <img src="./images/drawers.jpg" alt="green drawers with vase on top" />
+    </div>
+
+    <div class="article-preview-text">
+        <h1>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
+        <p>Ever been in a room and felt like something was missing? Perhaps it felt slighly bare and uninviting. I've got some simple tips to help you make any room feel complete.</p>
+    </div>
+
+    <footer class="article-footer">
+        <img src="./images/avatar-michelle.jpg" alt="Michelle Appleton" />
+            <div class="article-author">
+                <h2>Michelle Appleton</h2>
+                <time datetime="2020-06-28">28 Jun 2020</time>
+            </div>
+        <button type="button" aria-label="share"><img src="./images/icon-share.svg" alt="" /></button>
+        
+        </footer>
+
+  </article>
 </main>
 
 <style>
+
+    *, :global(body) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :root{
+        font-size: 13px;
+        --very-dark-greyish-blue: hsl(217, 19%, 35%);
+        --desaturated-dark-blue: hsl(214, 17%, 51%);
+        --grayish-blue: hsl(212, 23%, 69%);
+        --light-grayish-blue: hsl(210, 46%, 95%);
+    }
+
+    :global(body){
+        font-family: 'Manrope', sans-serif;
+        font-style: normal;
+    }
   
 </style>
