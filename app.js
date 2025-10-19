ea1046248b79d3d50bd66c0237e042dba8152317
AdamMatulay <adammatulay@proton.me>
2025-10-19 21:25:09 +0200

    I dont know

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const shareButton = document.getElementById('share-button');
-const shareOverlay = document.getElementById('share-overlay');
+const articleAuthor = document.querySelector('.article-author');
 const articleFooter = document.querySelector('.article-footer');
+const shareOverlay = document.getElementById('share-overlay');
 
-
-
-export function toggleShareOverlay() {
-    shareButton.addEventListener('click', toggleShareOverlay);
-    articleFooter.classList.toggle('active');
-    console.log('Share Overlay Activated');
+function toggleShareOverlay() {
+    articleFooter.classList.toggle('footer-active');
+    articleAuthor.classList.toggle('hidden');
+    shareOverlay.classList.toggle('active');
 }
+
+shareButton.addEventListener('click', toggleShareOverlay);
